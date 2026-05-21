21edab1bf87d741b7380fb222a3e570ead41d079
Rihabe Mounaim <rihabatimounaim@gmail.com>
2026-05-21 12:45:01 +0200

    Add validation for required fields in response

--- orchestration/validator.py
+++ orchestration/validator.py
@@ -1,1 +1,22 @@
+REQUIRED_FIELDS = [
+    "Feedback_ID",
+    "Main_Category",
+    "Customer_Sentiment",
+    "Issue_Summary",
+    "Order_Reference_ID",
+    "Product_Reference",
+    "Suggested_Department",
+    "Actionable_Resolution_Points"
+]
 
+
+def validate_response(response: dict) -> bool:
+    """
+    Validates that all required JSON fields exist.
+    """
+
+    for field in REQUIRED_FIELDS:
+        if field not in response:
+            return False
+
+    return True
